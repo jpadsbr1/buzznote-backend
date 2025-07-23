d84e670967d8e38bbc9fde538ffbab88291f87cd
João Pedro Alves da Silva <pingulim.br@gmail.com>
2025-07-23 02:02:54 -0300

    feat: add environment variables to .env

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -7,13 +7,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCaZo8ZbqSSH4yR9ltc1seZVOLlYVAFnq0",
-  authDomain: "buzznote-7cd95.firebaseapp.com",
-  projectId: "buzznote-7cd95",
-  storageBucket: "buzznote-7cd95.firebasestorage.app",
-  messagingSenderId: "1087790840110",
-  appId: "1:1087790840110:web:58578c5ecd077b0928a2bf",
-  measurementId: "G-HSN65R5MG5"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
